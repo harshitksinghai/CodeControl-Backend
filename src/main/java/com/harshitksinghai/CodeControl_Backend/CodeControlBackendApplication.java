b95b364dcff7e12063cf6c1e447bbea21265f4a6
HarshitSinghai <harshitksinghai.official@gmail.com>
2024-11-23 13:48:19 +0530

    update application.yaml, add database configuration and httpcookie configurations for jwt and refreshtoken cookies

---
+++ src/main/java/com/harshitksinghai/CodeControl_Backend/CodeControlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.harshitksinghai.CodeControl_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeControlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeControlBackendApplication.class, args);
+	}
+
+}
