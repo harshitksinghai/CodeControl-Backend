86325e8c0bba55bdd367ee6c3d2cec70b79011f3
HarshitSinghai <harshitksinghai.official@gmail.com>
2024-12-09 23:23:58 +0530

    auth done for now - oauth remaining, and some tweeks too

--- src/main/java/com/harshitksinghai/CodeControl_Backend/Config/WebConfig.java
+++ src/main/java/com/harshitksinghai/CodeControl_Backend/Config/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 import java.util.Arrays;
 
 @Configuration
